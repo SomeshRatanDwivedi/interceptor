612011d337b0672e3f86582ebac29a03de73d915
Somesh ratan dwivedi <ratandubeysomesh@gmail.com>
2022-07-07 17:42:36 +0530

    somesh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'poc';
+  
+  constructor( private http: HttpClient ){}
+  onSubmit(userInfo:any){
+    const data={
+      "email":userInfo.email,
+      "password":userInfo.password
+    }
+    this.http.post('https://reqres.in/api/login', data).subscribe((res:any)=>{
+      console.log("called")
+      if(res && res.token){
+        localStorage.setItem('mytoken', res.token);
+      }
+    },
+    error=>{
+      console.log("err", error);
+    })
+  }
+  showToken(){
+      console.log(localStorage.getItem("mytoken"))
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { LoggingInterceptor } from './logging.interceptor';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
+
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: LoggingInterceptor, multi:true} 
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
